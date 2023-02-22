ae851f9ead242189c0d7ebaf79ddf685088e4b1d
Plain Wu <plain.wu@lig.com.tw>
2023-02-22 16:24:04 +0800

    Upgrade scanner sdk 3.2.1

--- app/src/main/java/tw/com/lig/sdk/player/MainActivity.kt
+++ app/src/main/java/tw/com/lig/sdk/player/MainActivity.kt
@@ -16,9 +16,7 @@ class MainActivity: Activity() {
     private var opened = false
 
     private val sLackPermissions = arrayOf(
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.CAMERA,
-        Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.RECORD_AUDIO
     )
 
